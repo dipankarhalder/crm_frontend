d3d8dd99f28fad1af04464eeadfe557001507448
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2025-01-30 21:07:06 +0530

    updated folder structure

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,7 +1,15 @@
-import { IChildren } from "@/interface";
+import { Outlet, Navigate } from "react-router-dom";
+
+import { useAuth } from "@/hooks/use-auth";
 import { AuthLogo } from "@/components/elements/auth/AuthLogo";
 
-export const AuthLayout = ({ heading, children }: IChildren) => {
+export const AuthLayout = () => {
+  const { user } = useAuth();
+
+  if (user) {
+    return <Navigate to="/dashboard" />;
+  }
+
   return (
     <section className="h-screen w-full flex items-center justify-center bg-blue-50">
       <div className="fixed top-0 right-0 h-screen w-[56%] bg-indigo-600"></div>
@@ -10,10 +18,10 @@ export const AuthLayout = ({ heading, children }: IChildren) => {
           <div className="flex flex-col text-center mb-6">
             <AuthLogo />
             <h1 className="text-xl font-semibold tracking-tight mb-0">
-              {heading}
+              skfdjhjksefd
             </h1>
           </div>
-          {children}
+          <Outlet />
         </div>
       </div>
     </section>
---
+++ src/pages/apps/VendorsPage.tsx
@@ -0,0 +1,3 @@
+export const VendorsPage = () => {
+  return <div>Vendors</div>;
+};
