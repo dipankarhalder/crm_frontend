94749f04b048d3878c99c609e4e6659278f3da72
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2025-01-31 21:22:43 +0530

    added vendor list

--- src/pages/apps/CustomersPage.tsx
+++ src/pages/apps/CustomersPage.tsx
@@ -1,11 +1,15 @@
-import { CustomerDataTable } from "@/components/elements/customer/CustomerTable";
+import { CustomerDataTable } from "@/components/elements/dataTable/CustomerTable";
 import { customerColumns } from "@/components/elements/customer/Columns";
-import { customerData } from "@/components/elements/customer/Cdata";
+import { customerData } from "@/components/elements/dataTable/Cdata";
 
 export const CustomersPage = () => {
   return (
     <div className="flex w-full">
-      <CustomerDataTable columns={customerColumns} data={customerData} />
+      <CustomerDataTable
+        columns={customerColumns}
+        data={customerData}
+        heading={"Manage list of customers"}
+      />
     </div>
   );
 };
