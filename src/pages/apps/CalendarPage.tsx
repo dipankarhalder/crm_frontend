b0cc2c2ed7e4d1ff1b0fd943b5fea3383473a0b9
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2025-01-31 22:09:36 +0530

    updated all the links

---
+++ src/pages/apps/CalendarPage.tsx
@@ -0,0 +1,7 @@
+export const CalendarPage = () => {
+  return (
+    <div>
+      <p>Calendar</p>
+    </div>
+  );
+};
--- src/router/links.ts
+++ src/router/links.ts
@@ -1,4 +1,9 @@
 export const applinks = {
   login: "/",
   register: "registration",
+  dashboard: "dashboard",
+  customers: "customers",
+  vendors: "vendors",
+  calendar: "calendar",
+  billing: "billing",
 };
