a2807b5e00b73d5bf9bc88f4a068b57eae345039
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2025-02-06 13:43:39 +0530

    updated API calls

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,18 @@ import "@/index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { router } from "./router";
 import { Toaster } from "@/components/ui/toaster";
 
+const queryClient = new QueryClient();
 const rootapp = document.getElementById("root");
+
 createRoot(rootapp!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-    <Toaster />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <Toaster />
+    </QueryClientProvider>
   </StrictMode>
 );
