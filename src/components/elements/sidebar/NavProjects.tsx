2961f01cc33bd0fe25e8968b5899ee14c1dbc451
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2025-02-25 20:21:03 +0530

    fixed sidebar link

--- src/components/elements/sidebar/NavProjects.tsx
+++ src/components/elements/sidebar/NavProjects.tsx
@@ -13,28 +13,22 @@ export function NavProjects({ projects }: any) {
   return (
     projects && (
       <SidebarGroup>
-        <SidebarGroupLabel>Last 4 Consumers</SidebarGroupLabel>
+        <SidebarGroupLabel>Last 6 Consumers</SidebarGroupLabel>
         <SidebarMenu>
-          {projects.slice(0, 4).map((item: any) => (
+          {projects.slice(0, 6).map((item: any) => (
             <SidebarMenuItem key={item._id}>
               <SidebarMenuButton asChild tooltip={item.name}>
                 <Link to={`customers/view/${item._id}`}>
                   <Box className="text-yellow-600" />
                   <span>{item.name}</span>
-                  <ChevronRight
-                    className="ml-auto transition-transform duration-200 text-gray-400"
-                    size={12}
-                  />
+                  <ChevronRight className="ml-auto transition-transform duration-200 text-gray-400" size={12} />
                 </Link>
               </SidebarMenuButton>
             </SidebarMenuItem>
           ))}
           <SidebarMenuItem>
             <SidebarMenuButton>
-              <Link
-                to={applinks.customers}
-                className="flex items-center w-full"
-              >
+              <Link to={applinks.customers} className="flex items-center w-full">
                 <MoreHorizontal className="text-slate-400 mr-2 w-4 h-4" />
                 <span className="text-xs text-slate-400 font-medium">More</span>
               </Link>
